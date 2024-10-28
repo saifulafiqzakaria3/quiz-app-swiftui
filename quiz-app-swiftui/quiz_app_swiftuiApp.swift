bd53c299b32750958c9de2505e8e071a17977ce8
Saiful Afiq <saifulafiqzakaria@yahoo.com>
2024-10-28 22:54:13 +0800

    [0001] Create new xcode project for swiftUI Quiz App

---
+++ quiz-app-swiftui/quiz_app_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  quiz_app_swiftuiApp.swift
+//  quiz-app-swiftui
+//
+//  Created by Saiful Afiq on 28/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct quiz_app_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
